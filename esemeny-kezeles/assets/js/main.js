6a1d04e52a8c5d82ac1d9df9ed5ef4db2f000e37
cherryApp <j.cserko@gmail.com>
2020-12-09 12:01:31 +0100

    Feat: switch case version.

--- esemeny-kezeles/assets/js/main.js
+++ esemeny-kezeles/assets/js/main.js
@@ -2,19 +2,42 @@
     // Megkeresem a szükséges elemeket.
     const clock = document.querySelector('.clock');
     const btnGroup = document.querySelector('.btn-group');
-    const playBtn = btnGroup.querySelector('.timer-start');
+    /* const playBtn = btnGroup.querySelector('.timer-start');
     const pauseBtn = btnGroup.querySelector('.timer-pause');
-    const resetBtn = btnGroup.querySelector('.timer-reset');
+    const resetBtn = btnGroup.querySelector('.timer-reset'); */
+
+    // Kattintás kezelő függvény.
+    const clockHandlerClick = (ev) => {
+        let btn = null;
+        if (ev.target.classList.contains('fa')) {
+            btn = ev.target.parentElement;
+        } else {
+            btn = ev.target;
+        }
+
+        const btnClass = btn.className.split('-').pop();
+        switch(btnClass) {
+            case 'start': isTiming = true;
+                break;
+            case 'pause': isTiming = false;
+                break;
+            case 'reset': 
+                currentTime = 0;
+                showTime();
+                break;
+        }
+    };
 
     // Események beállítása.
     let isTiming = false;
     let currentTime = 0;
-    playBtn.addEventListener('click', () => isTiming = true );
+    btnGroup.addEventListener( 'click', clockHandlerClick );
+    /* playBtn.addEventListener('click', () => isTiming = true );
     pauseBtn.addEventListener('click', () => isTiming = false );
     resetBtn.addEventListener('click', () => {
         currentTime = 0;
         showTime();
-    });
+    }); */
 
     // Idő megjelenítése.
     const showTime = () => {
