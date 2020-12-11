ce59462825742921a2d88ef02dbbd883fd7759bd
cherryApp <j.cserko@gmail.com>
2020-12-11 10:54:11 +0100

    Feat: load and show todos.

--- 9-todo-project/assets/js/main.js
+++ 9-todo-project/assets/js/main.js
@@ -1,14 +1,13 @@
 (function() {
     // Mock data.
-    let todos = [
-        {title: 'Lunch', content: 'Lunch with my friends'},
-        {title: 'Lunch', content: 'Lunch with my friends'},
-        {title: 'Lunch', content: 'Lunch with my friends'}
-    ];
+    let todos = [];
     
     // Parts of date.
     const bodyDay = document.querySelector('.body__day');
     const bodyDate = document.querySelector('.body__date');
+    const todoAddBtn = document.querySelector('.todo__btn');
+    const todoInput = document.querySelector('.todo__input');
+    const todoListPending = document.querySelector('.todo__list--pending');
     
     const dayNames = [
         'Sunday', 
@@ -44,30 +43,76 @@
 
     // Initialize application.
     const init = () => {
+        showDate();
+        setListeners();
+        loadExistingTodos();
+    };
+
+    // Load existing todos.
+    const loadExistingTodos = () => {
         const savedTodos = localDB.getItem('todos');
         if (savedTodos) {
             todos = savedTodos;
         }
-
-        showDate();
+        
+        if (todos && Array.isArray(todos)) {
+            todos.forEach( todo => showTodo(todo) );
+        }
     };
 
     // Show date.
     const showDate = () => {
         const currentDate = new Date();
         const day = [
-            currentDate.getFullYear(), 
             currentDate.getMonth() + 1, 
-            currentDate.getDate()
+            currentDate.getDate(),
+            currentDate.getFullYear(), 
         ].map( num => num < 10 ? `0${num}` : num );
 
         bodyDay.textContent = dayNames[currentDate.getDay()];
         bodyDate.textContent = day.join('-');
     };
 
+    // Set event listeners.
+    const setListeners = () => {
+        todoAddBtn.addEventListener('click', addNewTodo);
+    };
 
+    // Save and add todo to the database.
+    const addNewTodo = () => {
+        const value = todoInput.value;
+        if (value === '') {
+            alert('Please type a todo.');
+            return;
+        }
 
+        const todo = {
+            text: value,
+            done: false
+        };
 
+        todos.push(todo);
+
+        localDB.setItem('todos', todos);
+
+        showTodo(todo);
+
+        todoInput.value = '';
+    };
+
+    // Show todo in the list.
+    const showTodo = todo => {
+        const todoItem = document.createElement('div');
+        todoListPending.appendChild(todoItem);
+
+        todoItem.innerHTML = `
+            <input type="checkbox">
+            <span>${todo.text}</span>
+            <button>
+                <i class="fa fa-trash"></i>
+            </button>        
+        `;
+    };
 
     init();
 })();
